25028e61218163092f65293a8dd095502a1d7809
ChristopherNHorsfall <christopher.horsfall@my.jcu.edu.au>
2022-09-23 10:18:19 +1000

    Refactor program to use functions

--- prac_02/password.py
+++
@@ -1,8 +0,0 @@
-"""
-"""
-min_length = int(input("Minimum Length:"))
-password = input("Password: ")
-while len(password) < min_length:
-    print("Password too short!")
-    password = input("Password: ")
-print("*" * len(password))
---
+++ prac_02/password_stars.py
@@ -0,0 +1,24 @@
+"""
+Turns passwords into a string of stars
+"""
+
+
+def main():
+    """Turns passwords into a string of stars"""
+    password = get_password()
+    print_stars(password)
+
+
+def print_stars(password):
+    """Print a string of stars equal to the length of the password"""
+    print("*" * len(password))
+
+
+def get_password():
+    """Get a valid password from user"""
+    min_length = int(input("Minimum Length:"))
+    password = input("Password: ")
+    while len(password) < min_length:
+        print("Password too short!")
+        password = input("Password: ")
+    return password
