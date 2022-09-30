2459928f8f223fb944ef6a688eebff4fd7b95c1f
jc939928 <christopher_horsfall@my.jcu.edu.au>
2022-09-30 11:13:16 +1000

    Wrote randoms.py

---
+++ prac_03/randoms.py
@@ -0,0 +1,28 @@
+"""
+
+"""
+import random
+
+# What did you see on line 1?
+# 8, 6, 18, 15, 8, 18
+# What was the smallest number you could have seen, what was the largest?
+# 5, 20
+# What did you see on line 2?
+# 7, 5, 9, 9, 3, 9
+# What was the smallest number you could have seen, what was the largest?
+# 3, 9
+# Could line 2 have produced a 4?
+# No, step = 2 prevents this.
+# What did you see on line 3?
+# 2.707556208724935
+# 4.848642305256221
+# 2.5700585551399286
+# 3.5261165029355057
+# 5.310011000433501
+# 3.3824204030684157
+# What was the smallest number you could have seen, what was the largest?
+# 2.5, 5.5
+
+# Prints random number from 1 to 100 inclusive
+# (numbers include both integers and floats)
+print(random.uniform(1, 100))
