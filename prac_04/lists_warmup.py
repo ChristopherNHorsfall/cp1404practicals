9802406ed335f39cbf06c60ba3f453d3a984ab6f
jc939928 <christopher_horsfall@my.jcu.edu.au>
2022-10-07 10:22:01 +1000

    Completed lists_warmup.py

---
+++ prac_04/lists_warmup.py
@@ -0,0 +1,25 @@
+"""
+
+"""
+
+numbers = [3, 1, 4, 1, 5, 9, 2]
+
+# 3
+# 2
+# 1
+# [3, 1, 4, 1, 5, 9]
+# [1,5] wrong, should be [1]
+# True
+# False
+# False
+# [3, 1, 4, 1, 5, 9, 2, 6, 5, 3]
+
+numbers[0] = 'ten'
+numbers[-1] = 1
+print(numbers[2:])
+print(numbers)
+print(9 in numbers)
+
+
+
+
