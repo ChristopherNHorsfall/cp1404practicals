bf7bca170e2c9a88a70ce9fed73d7929c9c7711a
Christopher_Horsfall <christopher.horsfall@my.jcu.edu.au>
2022-10-12 20:51:46 +1000

    Completed quick_picks.py

--- prac_04/quick_picks.py
+++ prac_04/quick_picks.py
@@ -10,27 +10,36 @@ VALUES_PER_LINE = 6
 
 
 def main():
+    """Creates a 'Quick Pick' lottery ticket based on user input"""
     number_of_lines = int(input("How many quick picks? "))
     lottery_ticket = generate_lottery_ticket(number_of_lines)
     display_lottery_ticket(lottery_ticket)
 
 
-def display_lottery_ticket(lottery_ticket):
-    for line in lottery_ticket:
-        for value in line:
-            print(f"{value:4}", end="")
-        print(" ")
-
-
 def generate_lottery_ticket(number_of_lines):
+    """Generates a lottery ticket based on the number of 'quick picks' (number_of_lines).
+    Each quick pick does not have repeated values and is sorted once created"""
     lottery_ticket = []
     for i in range(number_of_lines):
+        available_numbers = list(range(MIN_NUMBER, MAX_NUMBER + 1))  # Recreated at each line
         line = []
         lottery_ticket.append(line)
         for j in range(VALUES_PER_LINE):
-            value = random.randint(MIN_NUMBER, MAX_NUMBER)
+            value = random.choice(available_numbers)
+            available_numbers.remove(value)
             line.append(value)
+        line.sort()
+
     return lottery_ticket
+
+
+def display_lottery_ticket(lottery_ticket):
+    """Displays lottery ticket neatly"""
+    for line in lottery_ticket:
+        # line.sort()
+        for value in line:
+            print(f"{value:4}", end="")
+        print(" ")
 
 
 main()
