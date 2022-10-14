1747c588958a04f506c028cee2526f42513d2b8c
jc939928 <christopher.horsfall@my.jcu.edu.au>
2022-10-14 11:36:46 +1000

    Completed hex_colours.py

---
+++ prac_05/hex_colours.py
@@ -0,0 +1,25 @@
+"""
+CP1404/CP5632 Practical
+Intermediate Exercises
+Hex Colours
+"""
+
+COLOUR_TO_CODE = {"Absolute Zero": "#0048ba", "Acid Green": "#b0bf1a", "AliceBlue": "#f0f8ff",
+                  "Alizarin crimson": "#e32636",
+                  "Amaranth": "#e52b50",
+                  "Amber": "#ffbf00",
+                  "Amethyst": "#9966cc",
+                  "AntiqueWhite": "#faebd7",
+                  "AntiqueWhite1": "#ffefdb",
+                  "AntiqueWhite2": "#eedfcc"}
+
+colour = input("Enter colour name: ").lower()
+lowercase_colour_to_code = {colour.lower(): code for (colour, code) in COLOUR_TO_CODE.items()}
+# print(lowercase_colour_to_code)
+while colour != "":
+    try:
+        print(f"'{colour}' is {lowercase_colour_to_code[colour]}")
+    except KeyError:
+        print(f"{colour} is not in Dictionary")
+    colour = input("Enter colour name: ")
+
