df1de6333dbff7ff917578fa482551c20aa78e98
Christopher_Horsfall <christopher.horsfall@my.jcu.edu.au>
2022-10-15 14:24:18 +1000

    Finished emails.py, check with solutions and added doc strings

--- prac_05/emails.py
+++ prac_05/emails.py
@@ -5,17 +5,28 @@ Emails
 
 
 def main():
+    """Create dictionary of emails to names using user input"""
     email_to_name = {}
-    # email = input("Email: ")
-    email = "christopher.horsfall@gmail.com"
-    full_name = extract_full_name(email)
-    # print(full_name)
-    email_to_name[email] = full_name
-    print(email_to_name)
+    email = input("Email: ")
+    while email != "":
+        full_name = extract_full_name(email)
+        is_full_name = input(f"Is your name {full_name}? (Y/n) ")
+        if is_full_name.upper() != "Y" and is_full_name != "":
+            full_name = input("Name: ")
+        email_to_name[email] = full_name
+        email = input("Email: ")
+    print("")
+    for email, name in email_to_name.items():
+        print(f"{name} ({email})")
 
 
 def extract_full_name(email):
+    """Extract full name from email address"""
     email_parts = email.split("@")
-    text = email_parts[0]
-    name_parts = text.split(".")
-    return (" ".join(name_parts)).title()
+    email_name = email_parts[0]
+    name_parts = email_name.split(".")
+    full_name = (" ".join(name_parts)).title()
+    return full_name
+
+
+main()
