6022b2ac6336e932bbae8963248191359b26a1b8
Christopher_Horsfall <christopher.horsfall@my.jcu.edu.au>
2022-10-15 12:47:58 +1000

    Moved code into functions in emails.py

--- prac_05/emails.py
+++ prac_05/emails.py
@@ -2,12 +2,20 @@
 CP1404/CP5632 Practical
 Emails
 """
-email_to_name = {}
-# email = input("Email: ")
-email = "christopher.horsfall@gmail.com"
-email_parts = email.split("@")
-text = email_parts[0]
-name_parts = text.split(".")
-full_name = (" ".join(name_parts)).title()
-print(full_name)
 
+
+def main():
+    email_to_name = {}
+    # email = input("Email: ")
+    email = "christopher.horsfall@gmail.com"
+    full_name = extract_full_name(email)
+    # print(full_name)
+    email_to_name[email] = full_name
+    print(email_to_name)
+
+
+def extract_full_name(email):
+    email_parts = email.split("@")
+    text = email_parts[0]
+    name_parts = text.split(".")
+    return (" ".join(name_parts)).title()
