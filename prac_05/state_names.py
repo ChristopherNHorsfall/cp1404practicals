8fdcb67e0bd9a58fa7148eb290fb21e8172929ba
Christopher_Horsfall <christopher.horsfall@my.jcu.edu.au>
2022-10-17 14:29:15 +1000

    Added explanatory note to state_names.py

--- prac_05/state_names.py
+++ prac_05/state_names.py
@@ -1,10 +1,9 @@
 """
 CP1404/CP5632 Practical
 State names in a dictionary
-File needs reformatting
 """
 
-# TODO: Reformat this file so the dictionary code follows PEP 8 convention
+
 CODE_TO_NAME = {"QLD": "Queensland", "NSW": "New South Wales", "NT": "Northern Territory", "WA": "Western Australia",
                 "ACT": "Australian Capital Territory", "VIC": "Victoria", "TAS": "Tasmania"}
 print(CODE_TO_NAME)
@@ -16,6 +15,7 @@ while state_code != "":
     except KeyError:
         print("Invalid short state")
     state_code = input("Enter short state: ")
+# Print codes and names
 for code, name in CODE_TO_NAME.items():
     print(f"{code:3} is {name}")
 
