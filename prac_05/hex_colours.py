ca60e92d9df6ce78186a4554296fcab7b9145670
Christopher_Horsfall <christopher.horsfall@my.jcu.edu.au>
2022-10-17 14:26:51 +1000

    Added explanatory note to hex_colours.py

--- prac_05/hex_colours.py
+++ prac_05/hex_colours.py
@@ -14,8 +14,8 @@ COLOUR_TO_CODE = {"Absolute Zero": "#0048ba", "Acid Green": "#b0bf1a", "AliceBlu
                   "AntiqueWhite2": "#eedfcc"}
 
 colour = input("Enter colour name: ").lower()
+# Change all keys to lowercase
 lowercase_colour_to_code = {colour.lower(): code for (colour, code) in COLOUR_TO_CODE.items()}
-# print(lowercase_colour_to_code)
 while colour != "":
     try:
         print(f"'{colour}' is {lowercase_colour_to_code[colour]}")
