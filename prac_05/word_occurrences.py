6b8ec0073b02ae8cd6e294a3109be8a55fff6e89
Christopher_Horsfall <christopher.horsfall@my.jcu.edu.au>
2022-10-17 14:50:54 +1000

    Added explanatory note to word_occurrences.py

--- prac_05/word_occurrences.py
+++ prac_05/word_occurrences.py
@@ -5,8 +5,8 @@ Estimate: 30mins
 Actual: 30min +
 """
 
-# text = input("Text: ")
-text = "this is a collection of words of nice words this is a fun thing it is"
+text = input("Text: ")
+# text = "this is a collection of words of nice words this is a fun thing it is"
 words = text.split()
 word_to_count = {}
 for word in words:
@@ -15,6 +15,7 @@ for word in words:
     except KeyError:
         word_to_count[word] = 1
 
+# Sort word_to_count
 word_to_count = dict(sorted(word_to_count.items()))
 
 max_length = max(len(word) for word in word_to_count)
