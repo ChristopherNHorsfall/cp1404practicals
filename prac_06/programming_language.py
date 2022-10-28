de38b5349f2a9ccccb55e42539f46d87b838d5ad
ChristopherNHorsfall <christopher.horsfall@my.jcu.edu.au>
2022-10-28 11:00:58 +1000

    Started languages.py and programming_language.py

---
+++ prac_06/programming_language.py
@@ -0,0 +1,6 @@
+"""CP1404/CP5632 Practical
+Estimated time: 20min"""
+
+class ProgrammingLanguage:
+    """Respresents a programming language object"""
+    def __init__(self, is_dynamic=True, is_):
